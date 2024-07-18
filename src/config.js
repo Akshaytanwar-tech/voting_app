d73db9aaf9c0316db34d864479dfdcccce8af716
akshay <akshaytanwar1701@gmail.com>
2024-07-18 13:00:42 +0530

    url change

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const config = {
-  Url: "http://localhost:5000",
+  // Url: "http://localhost:5000",
+  Url: "https://voting-app-rk9x.onrender.com",
   token: localStorage.getItem("token"),
   urlencoded: "application/x-www-form-urlencoded",
   json: "application/json",
